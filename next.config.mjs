603bc4fea96e5d99694d5e48417b6d03be6fd9e2
dorinelrushi <rushidorinel@gmail.com>
2024-10-27 00:35:40 +0200

    ks

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,6 @@ const nextConfig = {
   images: {
     domains: ["img.clerk.com", "images.clerk.dev"],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "frame-src 'self' https://publisher.linkvertise.com;",
-          },
-        ],
-      },
-    ];
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: "25mb", // You can adjust the size limit as needed
