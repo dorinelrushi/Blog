6e403bd69aa360ec075ae3e7770e4c2b16b151be
dorinelrushi <rushidorinel@gmail.com>
2024-10-31 23:28:43 +0100

    ls pr

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,7 +30,28 @@ export const metadata = {
     "AI",
   ],
 
-  metadataBase: new URL("https://devpromote.online/"),
+  openGraph: {
+    title: "Dev Promote",
+    description: "Promoting Talents",
+    url: "https://devpromote.online/",
+    siteName: "Dev Promote",
+    images: [
+      {
+        url: "/web.png", // Replace with your actual image path
+        width: 1200,
+        height: 630,
+        alt: "Dev Promote - Promoting Talents",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Dev Promote",
+    description: "Promoting Talents",
+    images: ["/web.png"], // Replace with your actual image path
+  },
 };
 
 export default function RootLayout({ children }) {
