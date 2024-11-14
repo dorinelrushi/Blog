ba15e56dc39e65e889b41d3dd428f30da66697e8
dorinelrushi <rushidorinel@gmail.com>
2024-11-14 15:54:20 +0100

    latest update api

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -8,13 +8,21 @@ export async function GET(req) {
     const items = await Item.find({});
     return new Response(JSON.stringify(items), {
       status: 200,
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*", // Allow all origins
+        "Access-Control-Allow-Methods": "GET, OPTIONS", // Specify allowed methods
+        "Access-Control-Allow-Headers": "Content-Type", // Specify allowed headers
+      },
     });
   } catch (error) {
     console.error("Error fetching data:", error);
     return new Response(JSON.stringify({ message: "Error fetching data" }), {
       status: 500,
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
     });
   }
 }
