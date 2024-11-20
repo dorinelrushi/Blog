8ed1fdfc7021c23842aadf335800dede85b664ce
dorinelrushi <rushidorinel@gmail.com>
2024-11-20 21:32:13 +0100

    photo

--- src/app/compoenents/WebDev.js
+++ src/app/compoenents/WebDev.js
@@ -6,13 +6,13 @@ import { HiArrowLongRight } from "react-icons/hi2";
 function WebDev() {
   return (
     <div>
-      <div className="flex flex-col lg:flex-row w-[90%] lg:w-[80%] m-auto mt-[50px] lg:mt-[120px] gap-[80px]">
+      <div className="flex flex-col lg:flex-row w-[90%] lg:w-[80%] m-auto mt-[50px] lg:mt-[120px] gap-[50px]">
         <div className="">
           <div>
-            <h2 className="text-[25px] mb-[19px] font-medium ">
+            <h2 className="text-[35px] leading-[46px] font-bold lg:leading-[65px] lg:text-[57px] mb-[19px] ">
               Web Design and Development
             </h2>
-            <p className="text-[18px] max-w-[850px] lg:max-w-[530px] ">
+            <p className="text-[17px] max-w-[850px] mt-[30px] lg:max-w-[550px] ">
               At DevPromote, we create customized and optimized websites
               designed to meet your unique needs and goals. Our focus is on
               crafting visually appealing, responsive designs that provide an
@@ -35,7 +35,7 @@ function WebDev() {
         <div>
           <Image
             src="/webdevelopment.png"
-            width={900}
+            width={1000}
             height={100}
             alt="100"
             className="rounded-[21px]"
